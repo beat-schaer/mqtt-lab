5a0ce9b9091e5e0c162bc76845443c11675f485a
Beat Schär <beat.schaer@bbv.ch>
2026-03-02 13:54:40 +0000

    Update devcontainer to 2.0
    * Added Boost JSON & Boost UUID.

--- .devcontainer/build-boost.sh
+++ .devcontainer/build-boost.sh
@@ -4,5 +4,5 @@ wget https://archives.boost.io/release/1.90.0/source/boost_1_90_0.tar.bz2
 tar -xjf boost_1_90_0.tar.bz2
 cd boost_1_90_0
 ./bootstrap.sh
-./b2 --with-mqtt5 stage
-./b2 --with-mqtt5 install
+./b2 --with-mqtt5 --with-json --with-uuid stage
+./b2 --with-mqtt5 --with-json --with-uuid install
